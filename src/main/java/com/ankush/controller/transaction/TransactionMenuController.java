8fa9034fb9b85c85d76604fa82c9687c7bd86c15
AnkushSupnar <ankushsupnar@gmail.com>
2021-12-26 00:13:56 +0530

    Commit on 25 December 2021 night

--- src/main/java/com/ankush/controller/transaction/TransactionMenuController.java
+++ src/main/java/com/ankush/controller/transaction/TransactionMenuController.java
@@ -1,9 +1,12 @@
 package com.ankush.controller.transaction;
 
+import com.ankush.config.SpringFXMLLoader;
 import com.ankush.config.StageManager;
+import com.ankush.view.FxmlView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,8 @@ import java.util.ResourceBundle;
 public class TransactionMenuController implements Initializable {
     @Autowired @Lazy
     private StageManager stageManager;
+    @Autowired
+    SpringFXMLLoader loader;
     @FXML private HBox MenuPaymentReceived;
     @FXML private AnchorPane mainPane;
     @FXML private HBox menuDailyBilling;
@@ -25,9 +30,15 @@ public class TransactionMenuController implements Initializable {
     @FXML private HBox menuViewBills;
     @FXML private HBox menuViewInvoice;
     private Pane pane;
-
+    private BorderPane rootPane;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        menuPurchaseInvoice.setOnMouseClicked(e->{
+            stageManager.switchScene(FxmlView.PURCHASEINVOICE);
+//            pane = loader.getPage("/fxml/transaction/PurchaseInvoice.fxml");
+//            rootPane = (BorderPane) mainPane.getParent();
+//            rootPane.setCenter(pane);
+        });
     }
 }
